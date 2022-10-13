6ffa5d71c6765adac077b6d44c0b1ffa9578ca8a
serg1971ch <https://github.com/serg1971ch/2022-08-otus-spring-shibanov>
2022-10-13 07:06:03 +0500

    insert eraly code into QuizDaoImpl

--- home_work1/src/main/java/ru/otus/spring/dao/QuizSimpleDaoImpl.java
+++ home_work1/src/main/java/ru/otus/spring/dao/QuizSimpleDaoImpl.java
@@ -14,7 +14,10 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 
 public class QuizSimpleDaoImpl implements QuizDao {
     String refSourseFile;
@@ -25,6 +28,7 @@ public class QuizSimpleDaoImpl implements QuizDao {
     final static int INDEX_ANSWER_FIRST = 2;
     final static int INDEX_ANSWER_SECOND = 5;
     final static int INDEX_ANSWER_THIRD = 8;
+    ArrayList<Quiz> quizzes = new ArrayList<>();
 //    DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 //    DocumentBuilder db;
 //
@@ -50,12 +54,12 @@ public class QuizSimpleDaoImpl implements QuizDao {
 //    Node node = nameFile.item(0);
 //    String filename = node.getTextContent();
 
-    private void getListQuiz(String[] columns, ArrayList<Quiz> quizzes) {
+//    private void getListQuiz(String[] columns, ArrayList<Quiz> quizzes) {
 //        for (int i = 1; i < 6; i++) {
 //            columns = lines.get(i).split(";", 10)
-        getListQuizes(quizzes, columns);
+//        getListQuizes(quizzes, columns);
 //        }
-    }
+//    }
 
     public QuizSimpleDaoImpl (String refSourseFile) {
         this.refSourseFile = refSourseFile;
@@ -63,27 +67,51 @@ public class QuizSimpleDaoImpl implements QuizDao {
 
     @Override
     public Quiz findById(int id) {
-        ArrayList<Quiz> quizzes = null;
-//        try (BufferedReader br = Files.newBufferedReader(Paths.get("home_work1/src/resource/" + filename))) {
-        try( BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(refSourseFile), StandardCharsets.UTF_8))){
-            String DELIMITER = ";";
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] columns = line.split(DELIMITER);
-                System.out.println("quiz[" + String.join(", ", columns) + "]");
-                getListQuizes(quizzes, columns);
-                this.getListQuiz(columns, quizzes);
-            }
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
+        quizzes = parseFile();
         return quizzes.get(id);
     }
 
-    private void getListQuizes(ArrayList<Quiz> quizzes, String[] columns) {
-        answerFirst = new Answer(columns[INDEX_ANSWER_FIRST], TypeResponse.WRONG);
-        answerSecond = new Answer(columns[INDEX_ANSWER_SECOND], TypeResponse.RIGHT);
-        answerThird = new Answer(columns[INDEX_ANSWER_THIRD], TypeResponse.WRONG);
-        quizzes.add(new Quiz(1, columns[INDEX_QUESTION], answerFirst, answerSecond, answerThird));
+    private ArrayList<Quiz> parseFile() {
+        List<String> lines = null;
+        String[] columns;
+//        String url = "home_work1/src/main/resources/quiz.csv";
+        try {
+            lines = Files.readAllLines(Paths.get(refSourseFile));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (int i = 1; i < 6; i++) {
+            columns = lines.get(i).split(";", 10);
+            answerFirst = new Answer(columns[INDEX_ANSWER_FIRST], TypeResponse.WRONG);
+            answerSecond = new Answer(columns[INDEX_ANSWER_SECOND], TypeResponse.RIGHT);
+            answerThird = new Answer(columns[INDEX_ANSWER_THIRD], TypeResponse.WRONG);
+            quizzes.add(new Quiz(i, columns[INDEX_QUESTION], answerFirst, answerSecond, answerThird));
+        }
+        return quizzes;
     }
+//    @Override
+//    public Quiz findById(int id) {
+//        ArrayList<Quiz> quizzes = null;
+//        try (BufferedReader br = Files.newBufferedReader(Paths.get("home_work1/src/resource/" + filename))) {
+//        try( BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(refSourseFile), StandardCharsets.UTF_8))){
+//            String DELIMITER = ";";
+//            String line;
+//            while ((line = reader.readLine()) != null) {
+//                String[] columns = line.split(DELIMITER);
+//                System.out.println("quiz[" + String.join(", ", columns) + "]");
+//                getListQuizes(quizzes, columns);
+//                this.getListQuiz(columns, quizzes);
+//            }
+//        } catch (IOException ex) {
+//            ex.printStackTrace();
+//        }
+//        return quizzes.get(id);
+//    }
+
+//    private void getListQuizes(ArrayList<Quiz> quizzes, String[] columns) {
+//        answerFirst = new Answer(columns[INDEX_ANSWER_FIRST], TypeResponse.WRONG);
+//        answerSecond = new Answer(columns[INDEX_ANSWER_SECOND], TypeResponse.RIGHT);
+//        answerThird = new Answer(columns[INDEX_ANSWER_THIRD], TypeResponse.WRONG);
+//        quizzes.add(new Quiz(1, columns[INDEX_QUESTION], answerFirst, answerSecond, answerThird));
+//    }
 }
