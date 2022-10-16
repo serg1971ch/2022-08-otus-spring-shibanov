b20a797133444d0e5319737f2565d67903eed72e
serg1971ch <https://github.com/serg1971ch/2022-08-otus-spring-shibanov>
2022-10-16 08:12:14 +0500

    Рефакторинг кода по блоку QuizDaoImpl

--- home_work1/src/main/java/ru/otus/spring/dao/QuizSimpleDaoImpl.java
+++ home_work1/src/main/java/ru/otus/spring/dao/QuizSimpleDaoImpl.java
@@ -1,19 +1,10 @@
 package ru.otus.spring.dao;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-
 import ru.otus.spring.domain.Answer;
 import ru.otus.spring.domain.Quiz;
 import ru.otus.spring.domain.TypeResponse;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.*;
-import java.nio.charset.StandardCharsets;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -21,47 +12,13 @@ import java.util.List;
 
 public class QuizSimpleDaoImpl implements QuizDao {
     String refSourseFile;
-    Answer answerFirst;
-    Answer answerSecond;
-    Answer answerThird;
     final static int INDEX_QUESTION = 0;
     final static int INDEX_ANSWER_FIRST = 2;
     final static int INDEX_ANSWER_SECOND = 5;
     final static int INDEX_ANSWER_THIRD = 8;
     ArrayList<Quiz> quizzes = new ArrayList<>();
-//    DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-//    DocumentBuilder db;
-//
-//    {
-//        try {
-//            db = dbf.newDocumentBuilder();
-//        } catch (ParserConfigurationException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    Document doc;
-//
-//    {
-//        try {
-//            doc = db.parse(new File(refSourseFile));
-//        } catch (SAXException | IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    NodeList nameFile = doc.getElementsByTagName("name");
-//    Node node = nameFile.item(0);
-//    String filename = node.getTextContent();
 
-//    private void getListQuiz(String[] columns, ArrayList<Quiz> quizzes) {
-//        for (int i = 1; i < 6; i++) {
-//            columns = lines.get(i).split(";", 10)
-//        getListQuizes(quizzes, columns);
-//        }
-//    }
-
-    public QuizSimpleDaoImpl (String refSourseFile) {
+    public QuizSimpleDaoImpl(String refSourseFile) {
         this.refSourseFile = refSourseFile;
     }
 
@@ -82,9 +39,9 @@ public class QuizSimpleDaoImpl implements QuizDao {
         }
         for (int i = 1; i < 6; i++) {
             columns = lines.get(i).split(";", 10);
-            answerFirst = new Answer(columns[INDEX_ANSWER_FIRST], TypeResponse.WRONG);
-            answerSecond = new Answer(columns[INDEX_ANSWER_SECOND], TypeResponse.RIGHT);
-            answerThird = new Answer(columns[INDEX_ANSWER_THIRD], TypeResponse.WRONG);
+            Answer answerFirst = new Answer(columns[INDEX_ANSWER_FIRST], TypeResponse.WRONG);
+            Answer answerSecond = new Answer(columns[INDEX_ANSWER_SECOND], TypeResponse.RIGHT);
+            Answer answerThird = new Answer(columns[INDEX_ANSWER_THIRD], TypeResponse.WRONG);
             quizzes.add(new Quiz(i, columns[INDEX_QUESTION], answerFirst, answerSecond, answerThird));
         }
         return quizzes;
